7ae785865911569cff3194850b91fd430168563e
Felix Ruwe <0288704@student.rocvantwente.nl>
2018-06-05 13:44:34 +0200

    commit to synch

--- ProjectAO-Wagenpark/ProjectAO-Wagenpark/App_Start/BundleConfig.cs
+++ ProjectAO-Wagenpark/ProjectAO-Wagenpark/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace ProjectAO_Wagenpark
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
